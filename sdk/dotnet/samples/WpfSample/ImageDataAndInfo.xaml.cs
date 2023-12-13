9928e30ba261912fe45da6822bf15cfdc478d245
John Goyette <John.Goyette@dentsplysirona.com>
2023-12-13 13:53:00 -0500

    Added method to convert 8-bit image

--- sdk/dotnet/samples/WpfSample/ImageDataAndInfo.xaml.cs
+++ sdk/dotnet/samples/WpfSample/ImageDataAndInfo.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using DSIO.Filters.Api.Sdk.Types.V1;
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace WpfSample
 {
@@ -54,7 +47,27 @@ namespace WpfSample
             openFileDialog.Filter = "Image files (*.png)|*.png";
             if (openFileDialog.ShowDialog() == true)
             {
-                ViewModel.UploadImageFileName = openFileDialog.FileName;
+                // Extract the full path of image file
+                var fileName = openFileDialog.FileName;
+
+                // If the image is 8-bit, then we need to scale it up to 12
+                if (Is8BitPng(fileName))
+                {
+                    // Determine a new filename to hold the upscaled image
+                    var imageScaledPath = System.IO.Path.Combine(
+                        System.IO.Path.GetDirectoryName(fileName),
+                        $"{openFileDialog.SafeFileName}-scaled.png");
+
+                    // Upscale the image
+                    if (Upscale8bitImage(fileName, imageScaledPath))
+                    {
+                        // If successful, we want to use the scaled image file instead of the selected file
+                        fileName = imageScaledPath;
+                    }
+                }
+
+                // Update the Viewmodel with path to image for upload
+                ViewModel.UploadImageFileName = fileName;
             }
         }
 
@@ -93,5 +106,70 @@ namespace WpfSample
             this.Close();
         }
 
+        /// <summary>
+        ///  Checks if an image is an 8-bit gray scale image
+        /// </summary>
+        /// <param name="filename">Full path of the image file</param>
+        /// <returns>true if image is 8-bit grayscale</returns>
+        private bool Is8BitPng(string filename)
+        {
+            // Create a BitmapSource representing the 8-bit input image
+            var decoder = BitmapDecoder.Create(new Uri(filename), 
+                BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+           return decoder.Frames[0].Format == PixelFormats.Gray8;
+        }
+
+        /// <summary>
+        /// Scales an image from 8 to 12 bits and saves the resulting image to indicated file.
+        /// </summary>
+        /// <param name="imageFileNameIn"></param>
+        /// <param name="imageFileNameOut"></param>
+        /// <returns>true if the image was upscaled successfully</returns>
+        private bool Upscale8bitImage(string imageFileNameIn, string imageFileNameOut)
+        {
+            // Create a decoder to read the input image file
+            BitmapDecoder decoder = BitmapDecoder.Create(new Uri(imageFileNameIn), 
+                BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            BitmapSource bitmapSource = decoder.Frames[0];
+
+            // Make sure the input source is Gray8
+            if (bitmapSource.Format != PixelFormats.Gray8)
+                return false;
+
+            // Extract dimensions of the input image
+            int width = bitmapSource.PixelWidth;
+            int height = bitmapSource.PixelHeight;
+
+            // Copy the 8-bit image pixel data to an array
+            int stride = width * (bitmapSource.Format.BitsPerPixel / 8);
+            byte[] pixel8 = new byte[width * height];
+            bitmapSource.CopyPixels(pixel8, stride, 0);
+
+            // Allocate a new pixel array to hold the 12-bit data
+            ushort[] pixel12 = new ushort[width * height];
+
+            // Multiply each pixel value by a factor of 16 (convert from 8-bit to 12-bit)
+            for (int i = 0; i < pixel12.Length; i++)
+            {
+                pixel12[i] = (ushort)(pixel8[i] << 4);
+            }
+
+            // Create a BitmapSource using the new 12-bit data
+            int stride12 = width*2;
+            var bitmapSourceOut = BitmapSource.Create(width, height, bitmapSource.DpiX, bitmapSource.DpiY,
+                PixelFormats.Gray16, null, pixel12, stride12);
+
+            // Create a PNG bitmap encoder to save the result
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmapSourceOut));
+
+            // Save the new 16-bit grayscale image
+            using (FileStream fileStream = new FileStream(imageFileNameOut, FileMode.Create))
+            {
+                encoder.Save(fileStream);
+            }
+
+            return true;
+        }
     }
 }
