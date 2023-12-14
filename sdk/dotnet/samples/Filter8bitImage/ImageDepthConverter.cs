12d56615f8fd6769af40e1eead66e7a496418b59
John Goyette <John.Goyette@dentsplysirona.com>
2023-12-14 12:34:01 -0500

    Added sample project Filter8bitImage

---
+++ sdk/dotnet/samples/Filter8bitImage/ImageDepthConverter.cs
@@ -0,0 +1,104 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Filter8bitImage
+{
+    public static class ImageDepthConverter
+    {
+        public static bool Convert8BitGrayTo12BitGray(string imageFile8bit, string imageFile12bit)
+        {
+            // Load the input image as an 8-bit image file
+            using (var imageIn = Image.Load<L8>(imageFile8bit))
+            {
+                var width = imageIn.Width;
+                var height = imageIn.Height;
+
+                // Clone the image as a 16-bit type so we have a new image to manipulate
+                using (var imageOut = imageIn.CloneAs<L16>())
+                {
+                    // ImageSharp automatically scales the pixels from 8-bit to 16-bit, but
+                    // we want to scale to 12-bit. So, we will overwrite the pixel data
+                    // manually using the 8-bit data and shifting by 4 bits only
+                    for (var y = 0; y < height; y++)
+                    {
+                        for (var x = 0; x < width; x++)
+                        {
+                            // Read the 8-bit pixel and shift by 4 bits (multiply by 16)
+                            var pixel = imageIn[x, y];
+                            imageOut[x, y] = new L16((ushort)(pixel.PackedValue << 4));
+                        }
+                    }
+
+                    // Save the 12-bit image as a 16-bit PNG
+                    imageOut.Save(imageFile12bit, new PngEncoder()
+                    {
+                        BitDepth = PngBitDepth.Bit16,
+                        ColorType = PngColorType.Grayscale
+                    });
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Convert16BitGrayTo8BitGray(Stream imageStream16bit, string imageFile8bit)
+        {
+            // Load the input image as an 16-bit image file
+            using (var imageIn = Image.Load<L16>(imageStream16bit))
+            {
+                var width = imageIn.Width;
+                var height = imageIn.Height;
+
+                // Clone the image as a 8-bit type so we have a new image to manipulate
+                using (var imageOut = imageIn.CloneAs<L8>())
+                {
+                    // Scale pixel data by shifting 8 bits
+                    for (var y = 0; y < height; y++)
+                    {
+                        for (var x = 0; x < width; x++)
+                        {
+                            // Read the 16-bit pixel and shift by 8 bits (divide by 256)
+                            var pixel = imageIn[x, y];
+                            imageOut[x, y] = new L8((byte)(pixel.PackedValue / 256));
+                        }
+                    }
+
+                    // Save the 8-bit image as a 8-bit PNG
+                    imageOut.Save(imageFile8bit, new PngEncoder()
+                    {
+                        BitDepth = PngBitDepth.Bit8,
+                        ColorType = PngColorType.Grayscale
+                    });
+                }
+            }
+
+            return true;
+        }
+
+        public static (UInt16,UInt16) GetMinMaxGrayScale(string filename)
+        {
+            UInt16 min = UInt16.MaxValue;
+            UInt16 max = 0;
+            using (var image = Image.Load<L16>(filename))
+            {
+                image.ProcessPixelRows(accessor =>
+                {
+                    for (int y = 0; y < accessor.Height; y++)
+                    {
+                        var pixelRow = accessor.GetRowSpan(y);
+
+                        for (int x = 0; x < pixelRow.Length; x++)
+                        {
+                            min = Math.Min(min, pixelRow[x].PackedValue);
+                            max = Math.Max(max, pixelRow[x].PackedValue);
+                        }
+                    }
+                });
+
+                return (min, max);
+            }
+        }
+    }
+}
---
+++ sdk/dotnet/samples/Filter8bitImage/Program.cs
@@ -0,0 +1,116 @@
+﻿/// This sample illustrates how to use the DSIO Filter Api with
+/// an 8-bit image. We assume that the 8-bit image data does not
+/// have a nonlinear map applied.
+/// 
+/// The 8-bit image is upscaled to 12-bits and a LutInfo object
+/// is constructed representing the 12-bit image.
+/// 
+/// The DSIO Filter is called and the resulting 16-bit image is
+/// scaled back to 8-bit and saved as a PNG
+/// 
+/// This sample uses a NuGet package from ImageSharp for image processing
+/// 
+
+using DSIO.Filters.Api.Sdk.Client.V1;
+using Filter8bitImage;
+
+
+// API Keys are long, modify Console to get around 254 char limit
+Console.SetIn(new StreamReader(Console.OpenStandardInput(8192)));
+
+Console.WriteLine("Sample console app for Modality Api");
+
+// Read credentials
+Console.Write("Enter your API Username: ");
+var username = Console.ReadLine();
+Console.Write("Enter your API Key: ");
+var apikey = Console.ReadLine();
+
+// Create service proxy and set credentials
+var service = new ServiceProxy();
+service.SetBasicAuthenticationHeader(username, apikey);
+
+
+// ServiceProxy HttpClient calls will throw exceptions when
+// unsuccessful. Handle exceptions and show errors
+try
+{
+    // Test availability
+    Console.WriteLine("Checking availability of service...");
+    var isAvailable = await service.IsServiceAvailable();
+    Console.WriteLine($"Modality Api V1 service isAvailable: {isAvailable}");
+
+    if (isAvailable)
+    {
+        Console.Write("Enter full path to an 8-bit PNG image: ");
+        var inputFilepath = Console.ReadLine();
+
+        // Check for existing file
+        if (File.Exists(inputFilepath))
+        {
+            // Create a filepath to store the converted image
+            var parentFolder = Path.GetDirectoryName(inputFilepath)!;
+            var fileName = Path.GetFileName(inputFilepath)!;
+            var scaled12Filepath = Path.Combine(parentFolder, $"{fileName}.scaled12.png");
+
+            // Convert the 8-bit image to 12-bit range and save as a 16-bit PNG
+            Console.WriteLine($"Converting image to 12 bits file {scaled12Filepath}...");
+            ImageDepthConverter.Convert8BitGrayTo12BitGray(inputFilepath, scaled12Filepath);
+
+            // Compute the min/max gray levels in 12-bit range
+            (var minGray, var maxGray) = ImageDepthConverter.GetMinMaxGrayScale(scaled12Filepath);
+
+            // create a stream from the 12-bit image file
+            var fileStream = new FileStream(scaled12Filepath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var imageContent = new StreamContent(fileStream);
+
+            // Create an ImageInfo describing the 12-bit image. We hardcode Binning to Unbinned in
+            // this sample and we set LutInfo to a linear map. Note that we reverse the min/max grays.
+            // This is done because the DSIO Filter Service will operate on inverted data. By swapping
+            // the min/max gray, we end up with the expected map instead of an inverted map
+            var imageInfo = new DSIO.Filters.Api.Sdk.Types.V1.ImageInfo
+            {
+                AcquisitionInfo = new DSIO.Filters.Api.Sdk.Types.V1.AcquisitionInfo
+                {
+                    Binning = DSIO.Filters.Api.Sdk.Types.V1.BinningMode.Unbinned
+                },
+                LutInfo = new DSIO.Filters.Api.Sdk.Types.V1.LutInfo
+                {
+                    Gamma = 1.0,
+                    Slope = 4096,
+                    Offset = 0,
+                    TotalGrays = 4096,
+                    MinimumGray = maxGray,
+                    MaximumGray = minGray
+                }
+            };
+
+            // Create an image resource
+            var imageResource = await service.UploadImage(imageInfo, imageContent, "image/png");
+
+            // Apply the filter with desired parameters
+            var parameters = new DSIO.Filters.Api.Sdk.Types.V1.SupremeFilterParameters
+            {
+                Task = DSIO.Filters.Api.Sdk.Types.V1.SupremeFilterParameters.TaskNames.General,
+                Sharpness = 70
+            };
+
+            Console.WriteLine($"Applying filter...");
+            using (var filteredStream = await service.SupremeFilter(imageResource.Id, parameters))
+            {
+                // Convert the result from 16-bit back down to 8-bit and save result
+                var outputFilepath = Path.Combine(parentFolder, $"{fileName}.filtered.png");
+
+                Console.WriteLine($"Saving result file {outputFilepath}...");
+                ImageDepthConverter.Convert16BitGrayTo8BitGray(filteredStream, outputFilepath);
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception encountered: {ex.Message}");
+}
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
